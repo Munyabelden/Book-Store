fd2313c43d00ae0859c58ae5485d93456d47c07e
Gracerue <graceruvimbo681@gamil.com>
2023-04-06 13:14:06 +0200

    Add a list of books

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
+import Books from './components/Books';
+import Category from './components/Categories';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      Hello, World
+      <Layout />
+      <Routes>
+        <Route path="/" element={ <Books />} />
+        <Route path="/category" element={ <Category />} />
+      </Routes>
     </div>
   );
 }
