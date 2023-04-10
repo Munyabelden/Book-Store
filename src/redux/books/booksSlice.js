eb04d5544edcc0d2d1970ea76fea36a1dacb2bff
Gracerue <munyaradzi045@gamil.com>
2023-04-10 12:08:58 +0200

    Add redux and slices

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+export const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.books = state.books.filter(book => book.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addBook, removeBook } = bookSlice.actions;
+
+export default bookSlice.reducer;
