d8f29601f93a8a7214f576d34f72fcc0b16e647f
Gracerue <munyaradzi045@gamil.com>
2023-04-11 13:47:14 +0200

    Combine react with redux

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types';
+import Button from './RemoveBookButton';
 
-const Book = ({ bookList }) => {
+const Book = ({ bookList, handleRemoveBook }) => {
   return (
     <ul>
       {bookList.map(book => {
-        return (<li key={book.id}>{book.title} by {book.author} <button key={book.id}>Delete</button></li>)
-      })};
+        return (<li key={book.item_id}>{book.title} by {book.author} <Button key={book.item_id} handleRemoveBook={handleRemoveBook} book={book} name="Delete" /></li>)
+      })}
     </ul>
   );
 }
 
-Book.proptype = {
+Book.propTypes = {
     bookList: PropTypes.array,
+    handleRemoveBook: PropTypes.func
 }
 
-export default Book
+export default Book;
